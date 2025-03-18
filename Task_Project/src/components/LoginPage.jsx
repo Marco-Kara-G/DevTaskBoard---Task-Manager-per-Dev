dcf53e23b4821ab110fd3646db66cf728c597aa7
Marco Grassi <marco.grassi.private@gmail.com>
2025-03-18 16:30:33 +0100

    add log-in form & fix style

--- Task_Project/src/components/LoginPage.jsx
+++ Task_Project/src/components/LoginPage.jsx
@@ -3,6 +3,8 @@ import "./LoginPage.css";
 
 export function LoginPage() {
   const [userInfo, setUserInfo] = useState(null);
+  const [signIn, setSignIn] = useState(false);
+  const [logIn, setLogIn] = useState(true);
 
   const handleInfoChange = (e) => {
     const { name, value } = e.target;
@@ -10,7 +12,6 @@ export function LoginPage() {
   };
 
   const addUser = async (e) => {
-    e.preventDefault();
     console.log(userInfo);
 
     try {
@@ -32,80 +33,150 @@ export function LoginPage() {
   return (
     <div className="main-container">
       <h2>Welcome</h2>
-      <form className="sign-in-form" onSubmit={addUser}>
-        <div className="sign-in-form-personal-info">
-          <div className="input-row">
-            <label htmlFor="user-name">Name: </label>
-            <input
-              type="text"
-              name="name"
-              id="user-name"
-              placeholder="First Name"
-              onChange={handleInfoChange}
-              required
-            />
-          </div>
-          <div className="input-row">
-            <label htmlFor="user-last-name">Last name:</label>
-            <input
-              type="text"
-              name="last_name"
-              id="user-last-name"
-              placeholder="Last Name"
-              onChange={handleInfoChange}
-              required
-            />
-          </div>
-          <div className="input-row">
-            <label htmlFor="user-birth-date">Birth Date: </label>
-            <input
-              type="date"
-              name="date_of_birth"
-              id="user-birth-date"
-              onChange={handleInfoChange}
-              required
-            />
-          </div>
-        </div>
 
-        <div className="sign-in-form-loginInfo">
-          <div className="input-row">
-            <label htmlFor="login-name">userName: </label>
-            <input
-              type="text"
-              name="username"
-              id="login-name"
-              placeholder="UserName"
-              onChange={handleInfoChange}
-              required
-            />
-          </div>
-          <div className="input-row">
-            <label htmlFor="user-email">Email: </label>
-            <input
-              type="email"
-              name="email"
-              id="user-email"
-              placeholder="example@something.com"
-              onChange={handleInfoChange}
-              required
-            />
-          </div>
-          <div className="input-row">
-            <label htmlFor="user-password">Password:</label>
-            <input
-              type="password"
-              name="password"
-              id="user-password"
-              placeholder="select your password..."
-              onChange={handleInfoChange}
-              required
-            />
-          </div>
-        </div>
-
-        <button type="submit">Sign-In</button>
-      </form>
+      <div className="form-container">
+        {signIn && (
+          <form
+            className={`sign-in-form ${signIn ? "" : "hidden"}`}
+            onSubmit={addUser}
+          >
+            <div className="sign-in-form-personal-info">
+              <div className="input-row">
+                <label htmlFor="user-name">Name: </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="user-name"
+                  placeholder="First Name"
+                  onChange={handleInfoChange}
+                  required
+                />
+              </div>
+              <div className="input-row">
+                <label htmlFor="user-last-name">Last name:</label>
+                <input
+                  type="text"
+                  name="last_name"
+                  id="user-last-name"
+                  placeholder="Last Name"
+                  onChange={handleInfoChange}
+                  required
+                />
+              </div>
+              <div className="input-row">
+                <label htmlFor="user-birth-date">Birth Date: </label>
+                <input
+                  type="date"
+                  name="date_of_birth"
+                  id="user-birth-date"
+                  onChange={handleInfoChange}
+                  required
+                />
+              </div>
+            </div>
+            <div className="sign-in-form-loginInfo">
+              <div className="input-row">
+                <label htmlFor="login-name">userName: </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="login-name"
+                  placeholder="UserName"
+                  onChange={handleInfoChange}
+                  required
+                />
+              </div>
+              <div className="input-row">
+                <label htmlFor="user-email">Email: </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="user-email"
+                  placeholder="example@something.com"
+                  onChange={handleInfoChange}
+                  required
+                />
+              </div>
+              <div className="input-row">
+                <label htmlFor="user-password">Password:</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="user-password"
+                  placeholder="select your password..."
+                  onChange={handleInfoChange}
+                  required
+                />
+              </div>
+            </div>
+            <button type="submit">Sign-In</button>{" "}
+            <div className="change-log-sign-page">
+              {" "}
+              <p>
+                Already got an Account?{" "}
+                <button
+                  className="fake-link"
+                  onClick={() => {
+                    setLogIn(true);
+                    setSignIn(false);
+                  }}
+                >
+                  Log In
+                </button>
+              </p>
+            </div>
+          </form>
+        )}
+        {logIn && (
+          <form className={`log-in-form ${logIn ? "" : "hidden"}`}>
+            <div className="input-row">
+              <label htmlFor="login-name">userName: </label>
+              <input
+                type="text"
+                name="username"
+                id="login-name"
+                placeholder="UserName"
+                required
+              />
+            </div>
+            <div className="input-row">
+              <label htmlFor="user-email">Email: </label>
+              <input
+                type="email"
+                name="email"
+                id="user-email"
+                placeholder="example@something.com"
+                required
+              />
+            </div>
+            <div className="input-row">
+              <label htmlFor="user-password">Password:</label>
+              <input
+                type="password"
+                name="password"
+                id="user-password"
+                placeholder="select your password..."
+                required
+              />
+            </div>
+            <button type="submit">Log In</button>
+            <div className="change-log-sign-page">
+              <p>
+                Do not have an Account?{" "}
+                <button
+                  className="fake-link"
+                  onClick={() => {
+                    setLogIn(false);
+                    setSignIn(true);
+                  }}
+                >
+                  Sign In
+                </button>
+              </p>
+            </div>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
