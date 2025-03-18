d287075f83751e15ba295ae748655480868f6f14
Marco Grassi <marco.grassi.private@gmail.com>
2025-03-18 13:20:57 +0100

    add style to LoginPage

--- Task_Project/src/components/LoginPage.jsx
+++ Task_Project/src/components/LoginPage.jsx
@@ -2,51 +2,64 @@ import "./LoginPage.css";
 
 export function LoginPage() {
   return (
-    <div>
+    <div className="main-container">
       <h2>Welcome</h2>
       <form className="sign-in-form">
         <div className="sign-in-form-personal-info">
-          <label htmlFor="user-name">Name: </label>
-          <input
-            type="text"
-            name="user-name"
-            id="user-name"
-            placeholder="First Name"
-          />
-          <label htmlFor="user-last-name">Last name:</label>
-          <input
-            type="text"
-            name="user-last-name"
-            id="user-last-name"
-            placeholder="Last Name"
-          />
-          <label htmlFor="user-birth-date">Birth Date: </label>
+          <div className="input-row">
+            <label htmlFor="user-name">Name: </label>
+            <input
+              type="text"
+              name="user-name"
+              id="user-name"
+              placeholder="First Name"
+            />
+          </div>
+          <div className="input-row">
+            <label htmlFor="user-last-name">Last name:</label>
+            <input
+              type="text"
+              name="user-last-name"
+              id="user-last-name"
+              placeholder="Last Name"
+            />
+          </div>
+          <div className="input-row">
+            <label htmlFor="user-birth-date">Birth Date: </label>
+            <input type="date" name="user-birth-date" id="user-birth-date" />
+          </div>
+        </div>
 
-          <input type="date" name="user-birth-date" id="user-birth-date" />
-        </div>
         <div className="sign-in-form-loginInfo">
-          <label htmlFor="login-name">userName: </label>
-          <input
-            type="text"
-            name="login-name"
-            id="login-name"
-            placeholder="UserName"
-          />
-          <label htmlFor="user-email">Email: </label>
-          <input
-            type="email"
-            name="user-email"
-            id="user-email"
-            placeholder="example@something.com"
-          />
-          <label htmlFor="user-password">Password:</label>
-          <input
-            type="password"
-            name="user-password"
-            id="user-password"
-            placeholder="select your password..."
-          />
+          <div className="input-row">
+            <label htmlFor="login-name">userName: </label>
+            <input
+              type="text"
+              name="login-name"
+              id="login-name"
+              placeholder="UserName"
+            />
+          </div>
+          <div className="input-row">
+            <label htmlFor="user-email">Email: </label>
+            <input
+              type="email"
+              name="user-email"
+              id="user-email"
+              placeholder="example@something.com"
+            />
+          </div>
+          <div className="input-row">
+            <label htmlFor="user-password">Password:</label>
+            <input
+              type="password"
+              name="user-password"
+              id="user-password"
+              placeholder="select your password..."
+            />
+          </div>
         </div>
+
         <button type="submit">Sign-In</button>
       </form>
     </div>
